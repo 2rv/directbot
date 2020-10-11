fc46c61806468009a175f136a1b53c91a6b9baff
2rv <>
2020-10-11 21:16:41 +0300

    updated props

--- apps/observe/src/main.ts
+++ apps/observe/src/main.ts
@@ -8,7 +8,12 @@ async function bootstrap() {
   await initDatabaseConnection();
   const browser = await Puppeteer.launch({
     headless: true,
-    args: ['--disable-web-security'],
+    args: [
+      '--disable-web-security',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-extensions',
+    ],
   });
   const moduleProps = { browser };
   await new LoginModule(moduleProps).login();
