ab887193d40ae6529236b20fd4dad007be4cfe99
2rv <>
2020-11-17 18:31:43 +0200

    updated

--- apps/observe/src/sms.js
+++ apps/observe/src/sms.js
@@ -4,15 +4,20 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app
-  .route('/webhooks/inbound-sms')
-  .get(handleInboundSms)
-  .post(handleInboundSms);
+app.post('/message', handleInboundSms);
 
-function handleInboundSms(request, response) {
-  const params = Object.assign(request.query, request.body);
-  console.log(params);
-  response.status(204).send();
+function handleInboundSms(req, res) {
+  console.log('req.body', req.body);
+  console.log('msgFrom', req.body.From);
+  console.log('msgBody', req.body.Body);
+
+  res.send(`
+    <Response>
+      <Message>
+        Hello ${req.body.From}. You said: ${req.body.Body}
+      </Message>
+    </Response>
+  `);
 }
 
 app.listen(process.env.PORT || 3000);
