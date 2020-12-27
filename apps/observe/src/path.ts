635768badeeaf694bf292832716b9743521f0df5
2rv <>
2020-12-27 02:38:49 +0200

    upd

--- apps/observe/src/path.ts
+++ apps/observe/src/path.ts
@@ -1,5 +1,5 @@
 export enum Path {
-  LOGIN = 'https://www.instagram.com/accounts/login',
+  LOGIN = 'https://www.instagram.com/accounts/login/?source=auth_switcher',
   DIRECT = 'https://www.instagram.com/direct/inbox/',
   REQUEST = 'https://www.instagram.com/direct/requests/',
 }
